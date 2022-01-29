e8d0ee43e7c6dbae24ad040f017dae12aa13450c
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2022-01-29 11:49:35 +0100

    Door implemented

---
+++ Assets/Scripts/Door.gd
@@ -0,0 +1,20 @@
+extends ChangeSceneTrigger
+
+var changeTimer = 0.2 #0.2
+var inside = false
+
+func _ready():
+	$DoorClosed.visible=true
+	$DoorOpened.visible=false
+	connect("body_entered",self,"_on_MoveToSceneTrigger_body_entered")
+
+func _process(delta):
+	if inside:
+		changeTimer-=delta
+		if changeTimer<=0:
+			var _a=get_tree().change_scene_to(goTo)
+
+func _on_MoveToSceneTrigger_body_entered(_body):
+	inside=true
+	$DoorClosed.visible=false
+	$DoorOpened.visible=true
