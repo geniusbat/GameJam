0d64337a69f552ebb1b843793362ebff4f4cef2d
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2022-01-30 11:48:23 +0100

    Enemy level 12 map design

--- Assets/Scripts/EnemyLevelScripts/Enemy level chage music.gd
+++ Assets/Scripts/EnemyLevelScripts/Enemy level chage music.gd
@@ -4,17 +4,22 @@ extends ClearRoom
 # var a = 2
 # var b = "text"
 
+var hasPlayed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Door.monitoring = false
 	PlayerInfo.backgroundMusic.stream=preload("res://Assets/Audio/BGM/corpusculo.mp3")
-	PlayerInfo.backgroundMusic.volume_db=-15
+	PlayerInfo.backgroundMusic.volume_db=-100
 	PlayerInfo.backgroundMusic.play()
 	pass # Replace with function body.
 	
 func RoomCleared():
 	$Door.monitoring = true
+	if (!$AudioStreamPlayer.playing and !hasPlayed):
+		$AudioStreamPlayer.play()
+		hasPlayed = true
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
