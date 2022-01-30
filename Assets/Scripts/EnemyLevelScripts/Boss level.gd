31743664eff746c08333215ddbd842da2cf09471
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2022-01-30 16:26:52 +0100

    Final boss music added

---
+++ Assets/Scripts/EnemyLevelScripts/Boss level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if PlayerInfo.backgroundMusic.stream!=preload("res://Assets/Audio/BGM/super_boss_ultimate (1).mp3"):
+		PlayerInfo.backgroundMusic.stream=preload("res://Assets/Audio/BGM/super_boss_ultimate (1).mp3")
+		PlayerInfo.backgroundMusic.volume_db=-25
+		PlayerInfo.backgroundMusic.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
